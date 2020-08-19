170d5cdda53f88561e024915652c8df57ce7b471
edem fernando <edem.fbc@gmail.com>
2020-08-19 13:51:23 -0300

    Atualizado, faltando remover clientes de rows e calcular a média de tempo

--- project.py
+++ project.py
@@ -1,5 +1,12 @@
 import random
-from datetime import datetime
+import time
+
+def transform_minute(hour, minute):
+    hourMinute = hour * 60 + minute
+    return hourMinute
+
+def calculate_diff_time(bigValue, smallValue):
+    return int(bigValue - smallValue)
 
 client = [
     [1, 'Edem', 0, 0], [2, 'Ronaldo', 0, 0], [3, 'Michael', 0, 0], [4, 'Fernanda', 0, 0],
@@ -14,7 +21,6 @@ menu = ' 1 - Atender Cliente\n 2 - Visualizar tempo médio da fila\n 3 - Visuali
 serviceMenu = ' 1 - Receber Dinheiro\n 2 - Troca de produto\n 3 - Devolução\n 4 - Finalizar Atendimento\n 5 - Voltar'
 counter = 0
 rows = []
-data = datetime.now()
 
 # iniciando tempo
 for clientName in client:
@@ -25,7 +31,8 @@ for clientName in client:
 
     if clientName[1] in rows:
         clientName.append(True)
-        print(clientName[1], ' - ', clientName[4])
+        timeNow = time.localtime()
+        clientName[2] = transform_minute(timeNow[3], timeNow[4])
 
 while counter < 10:
     print("========== Atendimento rápido ==========")
@@ -51,7 +58,8 @@ while counter < 10:
 
         for i in client:
             if clientSelected == i[0]:
-                print("O tempo médio de", i[1], 'foi de', i[3], 'minutos')
+                #print("O tempo médio de", i[1], 'foi de', (i[2] - i[3]), 'minutos')
+                print(i[1], ' - ', i[2], ' - ', i[3])
         continue
 
     if actionMenu == 1:
@@ -89,22 +97,21 @@ while counter < 10:
                 continue
 
             if serviceSelected == 4:
-                # tempo finalizado"""
+                # tempo finalizado
                 for clientName in client:
                     if rows[0] == clientName[1]:
-                        clientName[3] = data.strftime("%M")
+                        finalTime = time.localtime()
+                        clientName[3] = transform_minute(finalTime[3], finalTime[4])
                         clientName[4] = False
+                        rows.pop(0)
+                        print(clientName[4])
 
                         if clientName[4] != False:
                             rows.append(clientName[1])
 
                         if len(rows) > 5:
                             break
-
-                rows.pop(0)
-                print(rows)
                 break
-
             counterMenuService = counterMenuService + 1
     counter = counter + 1
 
